5730b5ac564f2a119e65a89335a53556587f9330
Atryom <238136@mail.ru>
2021-07-29 14:03:53 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -1,15 +1,31 @@
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 6,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
+try {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+} catch {
+  log
+}
+
+const menuBtn = document.querySelector('.menu-btn');
+const navPanel = document.querySelector('.navbar-panel');
+const closeBtn = document.querySelector('.close-menu');
+
+menuBtn.addEventListener('click', () => {
+  navPanel.classList.toggle('is-open');
 });
-var swiper2 = new Swiper(".mySwiper2", {
-  loop: true,
-  spaceBetween: 10,
-  thumbs: {
-    swiper: swiper,
-  },
+
+closeBtn.addEventListener('click', () => {
+  navPanel.classList.remove('is-open');
 });
